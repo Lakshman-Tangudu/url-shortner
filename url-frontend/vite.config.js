e2d43c167b138f19d60169d5449bd690320fa57d
Tangudu Lakshman <tangudulakshman123@gmail.com>
2025-08-09 23:06:27 +0530

    updated vite.config

--- url-frontend/vite.config.js
+++ url-frontend/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist' // Ensure Vite outputs here (default, but explicit is safer)
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 });
